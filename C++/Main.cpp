ae8c01147f208e99daa5f62348f4f7fd1714ab97
Olamide <Olamideajumobi@gmail.com>
2021-01-14 09:38:06 +0100

    datatypes cont.. and their methods

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
+#define cway 5;//define constant in  c also called macros
+using namespace std;// or using std::cout to use only cout
+ enum cppenum{
+	peter,
+	louis,
+	stewie,
+	megan,
+	chris,
+	brian
+};
+// you have to declare and define a function before you call/use it else declare it first at the top and use wherever you want
 double powere(double base, int power)
 {
 	double start = 1;
@@ -12,22 +22,49 @@ double powere(double base, int power)
 }
 int main()
 {
+	cout << cway;
+	cout << "\nuse math expressions\n";
+	cout << sqrt(25)<<"\n";
+	cout << "the difference between remainder fn and modulo " << 15 % 2 << " and " << remainder(12.5, 2.0)<<endl;// remainder can take float and double datatypes
+	cout << fmax(10, 100) <<" and "<< fmin(12, 65)<<endl;//prints the max and min numbers
+	cout << trunc(-1.5) << " just removes the decimal value\n";
 	int y, power;
 	double base;
+	long double a = 300000000000000000000000.0;//this will show exponential value
+	cout << a << "\n";
+	cout << fixed << a << "\n";// this will show the actual value with exponents
+	const int constant = 12;
 	bool negative = false;
+	char s = 'a';
+	char name[] = "Olamide";
+	string str = "test";
+	str = str + " lets";
+	s = str[0];
+	int str1 = str.length();
+	int str2 = string("test").length();
 	bool positive = -1;
-	cout << sizeof(int) << endl;
-	cout << SHRT_MIN << endl;
-	cout << USHRT_MAX << endl;
+	cout << FLT_DIG << " is the amount of digits you can trust a float"<< endl;
+	cout << DBL_DIG << " is the amount of digits you can trust a double"<< endl;
+	cout << LDBL_DIG << " is the amount of digits you can trust a long double"<< endl;
+	cout << "\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a"; // cout  is equivalent of console.writeline
+	cout << sizeof(int) << endl;//sizeof(int) gets the number of bytes in an int
+	cout << SHRT_MIN << endl; //gets the minimum value of a short type
+	cout << USHRT_MAX << endl;//gets the maximum value of a unsigned short type
 	cout << sizeof(long long) << endl;
-	cout << "positive is: " << boolalpha << positive << " and negative is: " << boolalpha << negative << ".\n";
+	cout << "\npositive is: " << boolalpha << positive << " and negative is: " << boolalpha << negative << ".\n";//boolapha prints out bit value in words i.e true/false
 	cout << "Enter your age: \n";
-	cin >> y;
-	cout << "I am a good boy and my age is " << y << endl;
+	cin >> y;// cin  is equivalent of console.readline
+	cout << "I am a good boy and my age is " << y << endl;// endl == \n 
 	cout << "Enter base " << endl;
 	cin >> base;
 	cout << "Enter power " << endl;
 	cin >> power;
 	cout << "Default power " << pow(base, power) << endl;
 	cout << "Custom power " << powere(base, power) << endl;
+	string teststring, teststring2;
+	cout << "Enter test string: \n";
+	cin >> teststring; //cin grabs only the first word (split by space)
+	cout << "using normal cin "+teststring+"\n";
+	//getline(cin, teststring2);
+	//cout <<"using getline "+ teststring2 + "\n";
 }
